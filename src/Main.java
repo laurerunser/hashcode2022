ca0ec5040e8e99bcb612a6d1b549dab283ab4246
Oceane <oceane.guinard@outlook.fr>
2022-02-24 19:16:36 +0100

    bloup

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     int nb_contrib;
@@ -21,6 +22,16 @@ public class Main {
 
         // if contributors exist : do
         // update skills + time available
+    void traitement(int max_time,   ArrayList<Project> projects){
+        projects.sort();
+        for(int current_time=0;current_time<max_time;current_time++){
+            for(int i=0;i<projects.size();i++){
 
+            }
+        }
+    
+    }
+
+   
     // write sol
 }
